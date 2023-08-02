7218ea7730743a7798172c962ebc2ad0ba42d755
DESKTOP-01L7GB2\tfl <tu734843818@yahoo.com>
2023-08-02 11:55:42 +0800

    优化图片检查

--- UnityImageCheck.py
+++ UnityImageCheck.py
@@ -1,6 +1,10 @@
+import tkinter
+
 from Myconfig import *
 from PIL import Image
 import os
+from tkinter import filedialog
+
 
 def CheckImageSize(filepath):
     try:
@@ -9,21 +13,30 @@ def CheckImageSize(filepath):
         img = Image.open(filepath)
         if os.stat(filepath).st_size > 89478485:
             raise Exception("Max Size");
-        if img.width % 4 != 0 or img.height % 4 !=0:
-            print( f"{bcolors.WARNING}{filepath}{bcolors.ENDC}'s size not multiple of 4 | {bcolors.FAIL}Width: {img.width % 4}, Height: {img.height % 4} Size: {os.stat(filepath).st_size/1024}KB{bcolors.ENDC}")
+        if img.width % 4 != 0 or img.height % 4 != 0:
+            print(
+                f"{bcolors.WARNING}{filepath}{bcolors.ENDC}'s size not multiple of 4 | {bcolors.FAIL}Width: {img.width % 4}, Height: {img.height % 4} Size: {round(os.stat(filepath).st_size / 1024, 2)}KB{bcolors.ENDC}")
     except Exception as e:
         print(f"{bcolors.FAIL}Can not open file {filepath}{bcolors.ENDC}")
 
+
 def LoopAllImage(Floderpath):
-    for root,ds,fs,in os.walk(Floderpath):
+    for root, ds, fs, in os.walk(Floderpath):
         for f in fs:
-            yield os.path.join(root,f)
-def test(path):
+            yield os.path.join(root, f)
+
+
+def Check(path):
     for i in LoopAllImage(path):
-        if "png" in i and "meta" not in i :
+        if "png" in i and "meta" not in i:
             CheckImageSize(i)
+
+
 if __name__ == '__main__':
+    root = tkinter.Tk()
+    root.withdraw()
+    path = filedialog.askdirectory()
+    print(path)
     ImageCount = 0
-    test("C:\\fangdong\simulator_landlord_GB\simulator_landlord\Game\Assets")
+    Check(path)
     print(ImageCount)
-    # CheckImageSize('E:\WorkLog\\20230731\\xiugai\\dt3_cj2_sl_4_1_4.png')

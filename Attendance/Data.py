4880a5f376cfd40159232bf465431949a9824f02
tu <tu734843818@yahoo.com>
2023-09-05 08:52:29 +0800

    修改优化

---
+++ Attendance/Data.py
@@ -0,0 +1,23 @@
+#所有记录列表
+AllDataList=[]
+#没有上班时间列表
+NoOnWorkTimeList=[]
+#没有下班记录列表
+NoOffWorkTimeList=[]
+#迟到记录列表
+LateList=[]
+#早退记录列表
+LeaveEarlyList=[]
+#被跳过的记录列表
+SkipedList=[]
+
+#需要包含的日期
+ContainInfoDate=[]
+#需要跳过的日期
+SkipInfoDate=[]
+
+NameKey = "A"
+JoinKey = "D"
+OnWorkKey = "E"
+OffWorkKey = "F"
+BeginNumber = 3
--- Attendance/Parse.py
+++ Attendance/Parse.py
@@ -2,6 +2,7 @@ import tkinter
 import sys
 import tkinter.ttk
 from tkcalendar import Calendar, DateEntry
+import Data as globaldata
 if sys.platform == 'WIN32':
     from Myconfig import *
 from openpyxl import load_workbook
@@ -11,36 +12,13 @@ import warnings
 attendanceSheetPath=""
 import windnd
 import ctypes
-#所有记录列表
-AllDataList=[]
-#没有上班时间列表
-NoOnWorkTimeList=[]
-#没有下班记录列表
-NoOffWorkTimeList=[]
-#迟到记录列表
-LateList=[]
-#早退记录列表
-LeaveEarlyList=[]
-#被跳过的记录列表
-SkipedList=[]
-
-#需要包含的日期
-ContainInfoDate=[]
-#需要跳过的日期
-SkipInfoDate=[]
-
-NameKey = "A"
-JoinKey = "D"
-OnWorkKey = "E"
-OffWorkKey = "F"
-BeginNumber = 3
 # 跳过或者添加特定日期
 def SkipDate(JoinDateTime:datetime):
-    for date in ContainInfoDate:
+    for date in globaldata.ContainInfoDate:
        if date == JoinDateTime:
            return False
-    for date in SkipInfoDate:
-        if date == SkipInfoDate:
+    for date in globaldata.SkipInfoDate:
+        if date == globaldata.SkipInfoDate:
             return True
 
     if JoinDateTime.weekday() == 6 or JoinDateTime.weekday() == 7:
@@ -56,42 +34,44 @@ def ParseAttendanceSheet(path):
     temp = load_workbook(path).active
     if temp is None:
         ctypes.windll.user32.MessageBoxW(0, "表格数据有问题 请检查", "错误", 0)
-    for index in range(BeginNumber,temp.max_row):
+    for index in range(globaldata.BeginNumber,temp.max_row):
         NowIndex = str(index)
-        name = temp[NameKey + NowIndex]
-        join = temp[JoinKey + NowIndex]
-        onwork = temp[OnWorkKey + NowIndex]
-        offwork = temp[OffWorkKey + NowIndex]
+        name = temp[globaldata.NameKey + NowIndex]
+        join = temp[globaldata.JoinKey + NowIndex]
+        onwork = temp[globaldata.OnWorkKey + NowIndex]
+        offwork = temp[globaldata.OffWorkKey + NowIndex]
         person=PersonData(name.value,join.value,onwork.value,offwork.value)
         person.print()
-        AllDataList.append(person)
+        globaldata.AllDataList.append(person)
 
         if SkipDate(person.JoinDateTime):
-            SkipedList.append(person)
+            globaldata.SkipedList.append(person)
             continue
 
         if  type(person.OnWorkTime) is str:
-            NoOnWorkTimeList.append(person)
+            globaldata.NoOnWorkTimeList.append(person)
         elif IsNormalDayBeLate(person):
-            LateList.append(person)
+            globaldata.LateList.append(person)
         if type(person.OffWorkTime) is str:
-            NoOffWorkTimeList.append(person)
+            globaldata.NoOffWorkTimeList.append(person)
         elif(IsNormalDayLeaveEarly(person)):
-            LeaveEarlyList.append(person)
+            globaldata.LeaveEarlyList.append(person)
 
 def printname(fiel):
     print(fiel)
 
-warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
-tk = Tk()
-right_frame = Frame(tk,bg="Grey",height=50,width=50)
-right_frame.grid(row=0, column=1,padx=20,pady=20)
-cal = Calendar(right_frame, font="Arial 14", selectmode='day', locale='zh_CN', disabledforeground='red',
-               cursor="hand1", year=2023, month=6, day=5)
-cal.pack(fill="both", expand=True)
-left_frame = Frame(tk,bg="Gray",height=50,width=50)
-left_frame.grid(row=0, column=0,padx=20,pady=20)
-if sys.platform == 'WIN32':
-    windnd.hook_dropfiles(left_frame,func=ParseAttendanceSheet)
-    windnd.hook_dropfiles(right_frame,func=printname)
-tk.mainloop()
+
+if __name__ == '__main__':
+    warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
+    tk = Tk()
+    right_frame = Frame(tk,bg="Grey",height=50,width=50)
+    right_frame.grid(row=0, column=1,padx=20,pady=20)
+    cal = Calendar(right_frame, font="Arial 14", selectmode='day', locale='zh_CN', disabledforeground='red',
+                   cursor="hand1", year=2023, month=6, day=5)
+    cal.pack(fill="both", expand=True)
+    left_frame = Frame(tk,bg="Gray",height=50,width=50)
+    left_frame.grid(row=0, column=0,padx=20,pady=20)
+    if sys.platform == 'WIN32':
+        windnd.hook_dropfiles(left_frame,func=ParseAttendanceSheet)
+        windnd.hook_dropfiles(right_frame,func=printname)
+    tk.mainloop()

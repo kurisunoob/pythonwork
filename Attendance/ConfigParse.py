05403b42d616a5941c893bf9df2e9fe1dcca7645
DESKTOP-01L7GB2\tfl <tu734843818@yahoo.com>
2023-09-05 18:30:46 +0800

    请假展示

--- Attendance/ConfigParse.py
+++ Attendance/ConfigParse.py
@@ -3,6 +3,7 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import Data as globaldata
 from persondata import *
+from ttkbootstrap.style import Bootstyle
 def SkipDate(JoinDateTime:datetime):
     for date in globaldata.ContainInfoDate:
         if date == JoinDateTime:
@@ -252,4 +253,77 @@ class Calculator(ttk.Frame):
             result = eval(f"{x}{op}{y}")
             self.digitsvar.set(result)
             self.reset_variables()
+
+class CollapsingFrame(ttk.Frame):
+    """A collapsible frame widget that opens and closes with a click."""
+
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.columnconfigure(0, weight=1)
+        self.cumulative_rows = 0
+
+
+    def add(self, child, title="", bootstyle=PRIMARY, **kwargs):
+        """Add a child to the collapsible frame
+
+        Parameters:
+
+            child (Frame):
+                The child frame to add to the widget.
+
+            title (str):
+                The title appearing on the collapsible section header.
+
+            bootstyle (str):
+                The style to apply to the collapsible section header.
+
+            **kwargs (Dict):
+                Other optional keyword arguments.
+        """
+        if child.winfo_class() != 'TFrame':
+            return
+
+        style_color = Bootstyle.ttkstyle_widget_color(bootstyle)
+        frm = ttk.Frame(self, bootstyle=style_color)
+        frm.grid(row=self.cumulative_rows, column=0, sticky=EW)
+
+        # header title
+        header = ttk.Label(
+            master=frm,
+            text=title,
+            bootstyle=(style_color, INVERSE)
+        )
+        if kwargs.get('textvariable'):
+            header.configure(textvariable=kwargs.get('textvariable'))
+        header.pack(side=LEFT, fill=BOTH, padx=10)
+
+        # header toggle button
+        def _func(c=child): return self._toggle_open_close(c)
+        btn = ttk.Button(
+            master=frm,
+            bootstyle=style_color,
+            command=_func
+        )
+        btn.pack(side=RIGHT)
+
+        # assign toggle button to child so that it can be toggled
+        child.btn = btn
+        child.grid(row=self.cumulative_rows + 1, column=0, sticky=NSEW)
+
+        # increment the row assignment
+        self.cumulative_rows += 2
+
+    def _toggle_open_close(self, child):
+        """Open or close the section and change the toggle button
+        image accordingly.
+
+        Parameters:
+
+            child (Frame):
+                The child element to add or remove from grid manager.
+        """
+        if child.winfo_viewable():
+            child.grid_remove()
+        else:
+            child.grid()
 
--- Attendance/Parse.py
+++ Attendance/Parse.py
@@ -6,6 +6,7 @@ attendanceSheetPath=""
 import windnd
 import ExcelParse
 import ConfigParse as config
+import Data as globaldata
 
 def printname(fiel):
     print(fiel)
@@ -18,14 +19,27 @@ def showdialog():
     window = ttk.Toplevel(title="Time")
     print(config.LeaveDataDialog(window))
     window.mainloop()
+x = 1
+group1 = 1
+def AddLeave():
+    global x
+    global  group1
+    for personal in globaldata.PersonalLeaveList:
+        ttk.Checkbutton(group1, text=f'{personal.Name}').pack(fill=X)
+    x = x+1
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
     tk = ttk.Window(themename="superhero",title="工具",size=[1000,500])
     ttk.Button(command=showdialog,bootstyle="success",text="添加病事年假").pack(anchor=ttk.N,pady=5,side=LEFT)
     ttk.Button(command=showcalc,bootstyle="success",text="打开计算器").pack(anchor=ttk.N,pady=5,side=RIGHT)
-    time=ttk.DateEntry(tk)
-    time.pack();
+
+    cf=config.CollapsingFrame(tk)
+    group1=ttk.Frame(cf,padding=10)
+    cf.add(child=group1,title="banner1")
+    ttk.Button(command=AddLeave,bootstyle="success",text="刷新").pack(anchor=ttk.S,pady=5,side=TOP)
+
+    cf.pack()
     right_frame = ttk.Frame(tk,bootstyle="info",height=50,width=50)
     right_frame.pack()
     left_frame = ttk.Frame(tk,bootstyle="info",height=50,width=50)

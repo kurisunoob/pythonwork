e644b04c2d359724b300592fa3f33e0faaa503c3
tu <tu734843818@yahoo.com>
2023-09-10 12:14:59 +0800

    格式处理 列表去重

--- Attendance/Parse.py
+++ Attendance/Parse.py
@@ -2,52 +2,65 @@ import sys
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import warnings
-attendanceSheetPath=""
 import windnd
 import ExcelParse
-import ConfigParse as config
+import UI as ui
 import Data as globaldata
+
+# globa data
+group1: ttk.Frame
+
+attendanceSheetPath = ""
+
 
 def printname(fiel):
     print(fiel)
 
+
 def showlist():
     global group1
     window = ttk.Toplevel(title="Time")
-    cf = config.CollapsingFrame(window)
+    cf = ui.CollapsingFrame(window)
     group1 = ttk.Frame(cf, padding=10)
     refreshleavelist()
     cf.add(child=group1, title="banner1")
     cf.pack()
     window.mainloop()
+
+
 def showcalc():
     window = ttk.Toplevel(title="Time")
-    print(config.Calculator(window))
+    print(ui.Calculator(window))
     window.mainloop()
+
+
 def showdialog():
     window = ttk.Toplevel(title="Time")
-    print(config.LeaveDataDialog(window))
+    print(ui.LeaveDataDialog(window))
     window.mainloop()
-group1:ttk.Frame
+
+
 def refreshleavelist():
     global group1
-    # for item in group1.winfo_children():
-    #     item.destroy()
+    for item in group1.winfo_children():
+        item.destroy()
     for personal in globaldata.PersonalLeaveList:
-        ttk.Button(group1, text=f'{personal.Name}',command=(lambda: globaldata.PersonalLeaveList.remove(personal))).pack(fill=X)
+        ttk.Button(group1, text=f'{personal.Name}',
+                   command=(lambda: globaldata.PersonalLeaveList.remove(personal))).pack(fill=X)
+
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
-    tk = ttk.Window(themename="superhero",title="工具",size=[1000,500])
-    ttk.Button(command=showdialog,bootstyle="success",text="添加病事年假").pack(anchor=ttk.N,pady=5,side=LEFT)
-    ttk.Button(command=showcalc,bootstyle="success",text="打开计算器").pack(anchor=ttk.N,pady=5,side=RIGHT)
-    ttk.Button(command=showlist,bootstyle="success",text="刷新").pack(anchor=ttk.S,pady=5,side=TOP)
+    tk = ttk.Window(themename="superhero", title="工具", size=[1000, 500])
+    ttk.Button(command=showdialog, text="添加病事年假").pack(anchor=ttk.N, pady=5, side=LEFT)
+    ttk.Button(command=showcalc, text="打开计算器").pack(anchor=ttk.N, pady=5, side=RIGHT)
+    ttk.Button(command=showlist, text="刷新").pack(anchor=ttk.S, pady=5, side=TOP)
 
-    right_frame = ttk.Frame(tk,bootstyle="info",height=50,width=50)
+    right_frame = ttk.Frame(tk, height=50, width=50)
     right_frame.pack()
-    left_frame = ttk.Frame(tk,bootstyle="info",height=50,width=50)
+    left_frame = ttk.Frame(tk, height=50, width=50)
     left_frame.pack()
     if sys.platform == 'win32':
-        windnd.hook_dropfiles(left_frame,func=ExcelParse.ParseAttendanceSheet)
-        windnd.hook_dropfiles(right_frame,func=printname)
+        windnd.hook_dropfiles(left_frame, func=ExcelParse.ParseAttendanceSheet)
+        windnd.hook_dropfiles(right_frame, func=printname)
     tk.mainloop()
--- Attendance/persondata.py
+++ Attendance/persondata.py
@@ -1,74 +1,133 @@
 import datetime
 import time
 
+
 class PersonData:
-    def __init__(self,name,JoinTime,OnTime,OffTime):
+    def __init__(self, name, JoinTime, OnTime, OffTime):
         self.Name = name
         self.JoinDateTime = StrToTime_Day(JoinTime)
         self.OnWorkTime = StrToTime_Hour(OnTime)
         self.OffWorkTime = StrToTime_Hour(OffTime)
-    Name=''
-    JoinDateTime=time.time()
-    OnWorkTime=time.time()
-    OffWorkTime=time.time()
+
+    Name = ''
+    JoinDateTime = time.time()
+    OnWorkTime = time.time()
+    OffWorkTime = time.time()
+
+    def __eq__(self, other):
+        return self.Name == other.Name and self.JoinDateTime == other.JoinDateTime and self.OnWorkTime == other.OnWorkTime and self.OffWorkTime == other.OffWorkTime
+
     def print(self):
-        print(f"{self.Name} : {type(self.Name)};{self.JoinDateTime} : {type(self.JoinDateTime)};{self.OnWorkTime} : {type(self.OnWorkTime)};{self.OffWorkTime} : {type(self.OffWorkTime)}")
+        print(
+            f"{self.Name} : {type(self.Name)};{self.JoinDateTime} : {type(self.JoinDateTime)};{self.OnWorkTime} : {type(self.OnWorkTime)};{self.OffWorkTime} : {type(self.OffWorkTime)}")
+
+
 class PersonalLeaveData:
-    def __init__(self, name:str, JoinTime:datetime, completetime:datetime, leavehours:int):
+    def __init__(self,
+                 name: str,
+                 JoinTime: datetime,
+                 completetime: datetime,
+                 leavehours: int):
         self.Name = name
-        self.LeaveTime = (JoinTime)
-        self.CompleteTime = (completetime)
-        self.LeaveHours = (leavehours)
-    Name=''
-    LeaveTime=time.time()
-    CompleteTime=time.time()
-    LeaveHours=1
+        self.LeaveTime = JoinTime
+        self.CompleteTime = completetime
+        self.LeaveHours = leavehours
+
+    Name = ''
+    LeaveTime = time.time()
+    CompleteTime = time.time()
+    LeaveHours = 1
+
+    def __eq__(self, other):
+        return (self.Name == other.Name and
+                self.LeaveTime == other.LeaveTime and
+                self.CompleteTime == other.LeaveTime and
+                self.LeaveTime == other.LeaveTime)
+
     def print(self):
-        print(f"{self.Name} : {type(self.Name)};{self.LeaveTime} : {type(self.LeaveTime)};{self.CompleteTime} : {type(self.CompleteTime)};{self.LeaveHours} : {type(self.LeaveHours)}")
+        print(
+            f"{self.Name} : {type(self.Name)};{self.LeaveTime} : {type(self.LeaveTime)};{self.CompleteTime} : {type(self.CompleteTime)};{self.LeaveHours} : {type(self.LeaveHours)}")
+
+
 class YearLeavaData:
-    def __init__(self, name:str, JoinTime:datetime, completetime:datetime, leavehours:int):
+    def __init__(self,
+                 name: str,
+                 JoinTime: datetime,
+                 completetime: datetime,
+                 leavehours: int):
         self.Name = name
-        self.LeaveTime = (JoinTime)
-        self.CompleteTime = (completetime)
-        self.LeaveHours = (leavehours)
-    Name=''
-    LeaveTime=time.time()
-    CompleteTime=time.time()
-    LeaveHours=1
+        self.LeaveTime = JoinTime
+        self.CompleteTime = completetime
+        self.LeaveHours = leavehours
+
+    Name = ''
+    LeaveTime = time.time()
+    CompleteTime = time.time()
+    LeaveHours = 1
+
+    def __eq__(self, other):
+        return (self.Name == other.Name and
+                self.LeaveTime == other.LeaveTime and
+                self.CompleteTime == other.LeaveTime and
+                self.LeaveTime == other.LeaveTime)
+
     def print(self):
-        print(f"{self.Name} : {type(self.Name)};{self.LeaveTime} : {type(self.LeaveTime)};{self.CompleteTime} : {type(self.CompleteTime)};{self.LeaveHours} : {type(self.LeaveHours)}")
+        print(
+            f"{self.Name} : {type(self.Name)};{self.LeaveTime} : {type(self.LeaveTime)};{self.CompleteTime} : {type(self.CompleteTime)};{self.LeaveHours} : {type(self.LeaveHours)}")
+
+
 class SickLeavaData:
-    def __init__(self, name:str, JoinTime:datetime, completetime:datetime, leavehours:int):
+    def __init__(self,
+                 name: str,
+                 JoinTime: datetime,
+                 completetime: datetime,
+                 leavehours: int):
         self.Name = name
-        self.LeaveTime = (JoinTime)
-        self.CompleteTime = (completetime)
-        self.LeaveHours = (leavehours)
-    Name=''
-    LeaveTime=time.time()
-    CompleteTime=time.time()
-    LeaveHours=1
+        self.LeaveTime = JoinTime
+        self.CompleteTime = completetime
+        self.LeaveHours = leavehours
+
+    Name = ''
+    LeaveTime = time.time()
+    CompleteTime = time.time()
+    LeaveHours = 1
+
+    def __eq__(self, other):
+        return (self.Name == other.Name and
+                self.LeaveTime == other.LeaveTime and
+                self.CompleteTime == other.LeaveTime and
+                self.LeaveTime == other.LeaveTime)
+
     def print(self):
-        print(f"{self.Name} : {type(self.Name)};{self.LeaveTime} : {type(self.LeaveTime)};{self.CompleteTime} : {type(self.CompleteTime)};{self.LeaveHours} : {type(self.LeaveHours)}")
+        print(
+            f"{self.Name} : {type(self.Name)};{self.LeaveTime} : {type(self.LeaveTime)};{self.CompleteTime} : {type(self.CompleteTime)};{self.LeaveHours} : {type(self.LeaveHours)}")
+
 
 def StrToTime_Day(strtime):
     if "-" not in strtime:
-        return datetime.datetime.strptime(strtime,'%Y%m%d')
+        return datetime.datetime.strptime(strtime, '%Y%m%d')
     else:
         return "-"
+
+
 def StrToTime_Hour(strtime):
     if "-" not in strtime:
-        return datetime.datetime.strptime(strtime,'%H:%M')
+        return datetime.datetime.strptime(strtime, '%H:%M')
     else:
         return "-"
 
-def IsNormalDayBeLate(person:PersonData):
+
+def IsNormalDayBeLate(person: PersonData):
     return CompareHours_IsBeLate(StrToTime_Hour("10:00"), person)
 
-def IsNormalDayLeaveEarly(person:PersonData):
-    return CompareHours_LeaveEarly(StrToTime_Hour("18:30"),person)
-def CompareHours_IsBeLate(LimitTime:datetime,persondate:PersonData):
-   return( persondate.OnWorkTime > LimitTime)
 
-def CompareHours_LeaveEarly(LimitTime:datetime,persondate:PersonData):
-    return( persondate.OffWorkTime < LimitTime)
+def IsNormalDayLeaveEarly(person: PersonData):
+    return CompareHours_LeaveEarly(StrToTime_Hour("18:30"), person)
 
+
+def CompareHours_IsBeLate(LimitTime: datetime, persondate: PersonData):
+    return persondate.OnWorkTime > LimitTime
+
+
+def CompareHours_LeaveEarly(LimitTime: datetime, persondate: PersonData):
+    return persondate.OffWorkTime < LimitTime

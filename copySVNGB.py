99e7e6f9b84ce8b68af3ecb53ee6b18a173a449c
tu <tu734843818@yahoo.com>
2023-11-30 16:52:43 +0800

    复制前先拿取最新分支

--- copySVNGB.py
+++ copySVNGB.py
@@ -4,17 +4,19 @@ import time
 
 
 def git_pull():
-    path= "D:\\simulator\\PureObject\\simulator_landlord_GB";
+    path = "D:\\simulator\\PureObject\\simulator_landlord_GB"
     os.chdir(path)
     self_commands = "git.exe pull --progress -v --no-rebase \"origin\""
     os.system(self_commands)
+
+
 def copy():
-    try :
+    try:
         localtime = time.localtime(time.time())
         Strftime = time.strftime("%Y-%m-%d %H:%M:%S", localtime)
         print("开始时间为 :", Strftime)
-        shutil.rmtree(destination_dir,True)
-        shutil.copytree(source_dir, destination_dir,dirs_exist_ok = True)
+        shutil.rmtree(destination_dir, True)
+        shutil.copytree(source_dir, destination_dir, dirs_exist_ok = True)
         print('Directory copied successfully.')
         localtime = time.localtime(time.time())
         Strftime = time.strftime("%Y-%m-%d %H:%M:%S", localtime)
@@ -22,10 +24,14 @@ def copy():
     except Exception as e:
         print('Directory not copied.')
         print(e)
-git_pull()
-source_dir = 'D:\\simulator\\PureObject\\simulator_landlord_GB\\ExDataHome\\Excel\\Excel'
-destination_dir = 'D:\\SVN\\trunk\\game\\simulator_landlord_GB\\ExDataHome\\Excel\\Excel'
-copy()
-source_dir = 'D:\\simulator\\PureObject\\simulator_landlord_GB\\Game\\Assets'
-destination_dir = 'D:\\SVN\\trunk\\game\\simulator_landlord_GB\\Game\\Assets'
-copy()
+if __name__ == '__main__':
+    git_pull()
+    source_dir = 'D:\\simulator\\PureObject\\simulator_landlord_GB\\ExDataHome\\Excel\\Excel'
+    destination_dir = 'D:\\SVN\\trunk\\game\\simulator_landlord_GB\\ExDataHome\\Excel\\Excel'
+    copy()
+    source_dir = 'D:\\simulator\\PureObject\\simulator_landlord_GB\\Game\\Assets'
+    destination_dir = 'D:\\SVN\\trunk\\game\\simulator_landlord_GB\\Game\\Assets'
+    copy()
+    path= "D:\\SVN\\trunk\\game\\simulator_landlord_GB";
+    self_commands = f"TortoiseProc.exe /command:commit /path:{path}"
+    os.system(self_commands)
